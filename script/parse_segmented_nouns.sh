abd5da66d6af54586ed16f3a316784fba482eea3
Philipp Gawlik <info@philipp-gawlik.de>
2017-03-22 15:02:24 +0100

    Adjusted segmentation parser script.

--- script/parse_segmented_nouns.sh
+++ script/parse_segmented_nouns.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # file: parse_segmented_nouns.sh
 
-python ../segmentation_parser/segmentation_parser/parser.py --verbose --in ../corpora/segments/compound_cleaned_random.txt --out ../corpora/trees/segmentation_parser.out --dot
+cd ../segmentation_parser/segmentation_parser/
+python parser.py --verbose --dot
+cd ../../script/
 
 echo
 echo Please go to the folder "corpora/dot" and perform \"dot -Tpng *.dot -O\" to generate .png graph pictures of the dot files in corpora/dot/. We won\'t do it now because it might take a while.
